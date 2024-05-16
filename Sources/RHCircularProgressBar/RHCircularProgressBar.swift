11810dd1290338a59052dc1508ae23bca43e6206
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-05-16 22:15:28 +0800

    Define `RHCircularProgressBarDelegate`

--- Sources/RHCircularProgressBar/RHCircularProgressBar.swift
+++ Sources/RHCircularProgressBar/RHCircularProgressBar.swift
@@ -1,2 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+public protocol RHCircularProgressBarDelegate: AnyObject {
+    func progressBar(_ progressBar: RHCircularProgressBar, completionRateWillUpdate rate: Int, currentBarProgress value: Float)
+    func progressBar(_ progressBar: RHCircularProgressBar, isDonetoValue: Bool, currentBarProgress value: Float)
+}

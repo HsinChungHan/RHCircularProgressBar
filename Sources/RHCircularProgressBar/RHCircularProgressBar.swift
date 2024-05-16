34456c5fbf8d9f2a1239e5fed259f8a57097b426
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-05-16 22:16:37 +0800

    In order to simplify logic of start angle so define `StartAngleType`  to describe start angle

--- Sources/RHCircularProgressBar/RHCircularProgressBar.swift
+++ Sources/RHCircularProgressBar/RHCircularProgressBar.swift
@@ -1,5 +1,33 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  RHCircularProgressBar.swift
+//  RHCircularProgressBarDemoApp
+//
+//  Created by Chung Han Hsin on 2024/4/13.
+//
+
+import Foundation
+import RHUIComponent
+import UIKit
+
+public enum StartAngleType {
+    case twelveClock
+    case threeClock
+    case sixClock
+    case nineClock
+    var angle: CGFloat {
+        switch self {
+        case .twelveClock:
+            return -CGFloat.pi / 2
+        case .threeClock:
+            return 0
+        case .sixClock:
+            return CGFloat.pi / 2
+        case .nineClock:
+            return CGFloat.pi
+        }
+    }
+}
+
 public protocol RHCircularProgressBarDelegate: AnyObject {
     func progressBar(_ progressBar: RHCircularProgressBar, completionRateWillUpdate rate: Int, currentBarProgress value: Float)
     func progressBar(_ progressBar: RHCircularProgressBar, isDonetoValue: Bool, currentBarProgress value: Float)

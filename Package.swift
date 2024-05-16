a94df1b16f94ab54505c9755f31a0c9787ea69d5
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-05-16 22:07:05 +0800

    Add package dependancies: `RHUIComponent` and `SnapKit`

--- Package.swift
+++ Package.swift
@@ -5,17 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "RHCircularProgressBar",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "RHCircularProgressBar",
             targets: ["RHCircularProgressBar"]),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/HsinChungHan/RHUIComponent.git",
+            branch: "main"),
+        .package(
+            url: "https://github.com/SnapKit/SnapKit",
+            branch: "main"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RHCircularProgressBar"),
+            name: "RHCircularProgressBar",
+            dependencies: ["RHUIComponent", "SnapKit"]),
         .testTarget(
             name: "RHCircularProgressBarTests",
             dependencies: ["RHCircularProgressBar"]),
